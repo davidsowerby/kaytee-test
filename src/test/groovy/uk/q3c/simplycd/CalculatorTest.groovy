ddedcf08ee724fb059da8365143eeb23c3b58b44
David Sowerby <david.sowerby@virgin.net>
2017-04-27 18:47:17 +0100

    1 - Fails unit test quality gate

--- src/test/groovy/uk/q3c/simplycd/CalculatorTest.groovy
+++ src/test/groovy/uk/q3c/simplycd/CalculatorTest.groovy
@@ -19,13 +19,13 @@ class CalculatorTest extends Specification {
         calculator.add(2, 3) == 5
     }
 
-    def "subtract"() {
-        expect:
-        calculator.subtract(3, 2) == 1
-    }
-
-    def "multiply"() {
-        expect:
-        calculator.multiply(7, 3) == 21
-    }
+//    def "subtract"() {
+//        expect:
+//        calculator.subtract(3, 2) == 1
+//    }
+//
+//    def "multiply"() {
+//        expect:
+//        calculator.multiply(7, 3) == 21
+//    }
 }
